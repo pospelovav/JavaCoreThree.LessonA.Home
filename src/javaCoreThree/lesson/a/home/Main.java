543bd9c879b0f24124245fec77e72a2f8fc5fef3
pospelovav <shnurokvtaige@yandex.ru>
2020-11-12 14:11:41 +0300

    class Swap метод, который меняет два элемента массива местами метод, который преобразует массив в ArrayList

--- src/javaCoreThree/lesson/a/home/Main.java
+++ src/javaCoreThree/lesson/a/home/Main.java
@@ -1,4 +1,30 @@
 package javaCoreThree.lesson.a.home;
 
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
+
+    public static void main (String[] args){
+        String[] arrStr = {"one", "two", "three"};
+        Swap<String> arr1 = new Swap<>(arrStr);
+        System.out.println(Arrays.toString(arrStr));
+        System.out.println(Arrays.toString(arr1.getArray()));
+        arr1.swapElementsArr(0, 2);
+        System.out.println(Arrays.toString(arr1.getArray()));
+
+        Integer[] arrInt = {1, 2, 3};
+        Swap<Integer> arr2 = new Swap<>(arrInt);
+        System.out.println(Arrays.toString(arrInt));
+        System.out.println(Arrays.toString(arr2.getArray()));
+        arr2.swapElementsArr(0, 2);
+        System.out.println(Arrays.toString(arr2.getArray()));
+
+        ArrayList<String> arL = new ArrayList<>();
+        arr1.toArrayList(arL);
+        arL.add("four");
+        System.out.println(arL);
+
+    }
 }
---
+++ src/javaCoreThree/lesson/a/home/Swap.java
@@ -0,0 +1,27 @@
+package javaCoreThree.lesson.a.home;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Swap<T> {
+    private T[] array;
+
+    public T[] getArray() {
+        return array;
+    }
+
+    public Swap (T[] array){
+        this.array = array;
+    }
+
+    public void swapElementsArr(int i, int j){  //меняет i-й элемент массива с j-ым местами
+        T buffer = this.array[j];
+        this.array[j] = this.array[i];
+        this.array[i] = buffer;
+    }
+
+    public void toArrayList(ArrayList<T> arrList){  //преобразует массив в ArrayList
+        Collections.addAll(arrList, this.array);
+    }
+
+}
